ea04d6e219bd44505c1d7c1c7ec2dba73d8dcb40
czapi03 <49277551+czapi03@users.noreply.github.com>
2019-05-06 21:31:19 +0200

    some

--- server/places-server.js
+++ server/places-server.js
@@ -1,57 +1,102 @@
-console.log( 'Places Server Script' );
+console.log('Places Server Script');
 
 // npm install express <- wenn nicht vorhanden
-var express = require( 'express' );
+var express = require('express');
 
-var fs = require( 'fs' );
-var bp = require( 'body-parser' );
+var fs = require('fs');
+var bp = require('body-parser');
 
 var app = express();
 
-var places = require( './my_modules/places' );
+var places = require('./my_modules/places');
 
-var server = app.listen( 26893, function() {
-  console.log( 'Places Server running on port 26893' );
+var server = app.listen(26893, function() {
+  console.log('Places Server running on port 26893');
 });
 
-app.use( function( request, response, next) {
-  response.setHeader( 'Access-Control-Allow-Origin', '*' );
-  response.setHeader( 'Access-Control-Allow-Methods', 'GET, DELETE, POST, PUT' );
-  response.setHeader( 'Content-Type', 'application/json' );
+app.use(function(request, response, next) {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET, DELETE, POST, PUT');
+  response.setHeader('Content-Type', 'application/json');
   next();
 });
-
+app.use(bp.urlencoded({
+  extended: true
+})); // for parsing application/x-www-form-urlencoded
 /*
 app.get('/', function( request, response ) {
   response.end( 'Ich bins, dein Server!' );
 });
 */
 
-app.get('/places/', function( request, response ) {
-  console.log( '-\nLade alle Orte' );
-  places.ladeJSON().then( places.ladeOrte ).then( function( orte ){ // orte -> ARRAY
+app.get('/places/', function(request, response) {
+  console.log('-\nLade alle Orte');
+  places.ladeJSON().then(places.ladeOrte).then(function(orte) { // orte -> ARRAY
     var responseObjekt = {
-      'places':orte
+      'places': orte
     }
-    response.end( JSON.stringify(responseObjekt) );
+    response.end(JSON.stringify(responseObjekt));
   });
 });
 
-app.delete('/places/:id', function( request, response ) {
-  console.log( '-\nStart entferne ID: ' +  request.params.id );
+app.post('/places/', function(request, response) {
+
+  places.ladeJSON().then(function(alleDaten) {
+    var alleorte = alleDaten.places;
+    var rlng = request.body.lng;
+    var rlat = request.body.lat;
+    var rtitle = request.body.title;
+
+    for (let i in alleorte) {
+
+      if ( (alleorte[i].title == rtitle) || (alleorte[i].lat == rlat && alleorte[i].lng == rlng)) {
+          return response.end(JSON.stringify({
+          "status": "error",
+          "code": 601,
+          "message": "Ort existiert bereits."
+        }));
+      }
+      alleorte.push(request.body);
+
+      places.speichereJSON(alleDaten);
+
+    }
+  });
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+app.delete('/places/:id', function(request, response) {
+  console.log('-\nStart entferne ID: ' + request.params.id);
 
   places.id = request.params.id;
 
   places.ladeJSON()
-    .then( places.entferneOrt.bind(places) )
-    .then( places.speichereJSON )
-    .then( function() {
-        var responseObjekt = {status:'success'};
-        response.end( JSON.stringify(responseObjekt) );
+    .then(places.entferneOrt.bind(places))
+    .then(places.speichereJSON)
+    .then(function() {
+      var responseObjekt = {
+        status: 'success'
+      };
+      response.end(JSON.stringify(responseObjekt));
     })
-    .catch( function() {
-      var responseObjekt = {status:'error',code:404,message:'...'};
-      response.end( JSON.stringify(responseObjekt) );
+    .catch(function() {
+      var responseObjekt = {
+        status: 'error',
+        code: 404,
+        message: '...'
+      };
+      response.end(JSON.stringify(responseObjekt));
     });
 
 });
--- src/js/myscript.js
+++ src/js/myscript.js
@@ -16,117 +16,168 @@ toastr.options = {
   "hideMethod": "fadeOut"
 }
 
-var createConfirm = function( lnk, onsuccess ) {
+var createConfirm = function(lnk, onsuccess) {
   $(lnk).hide();
-  $( '<span>' )
-    .insertAfter( $(lnk) )
+  $('<span>')
+    .insertAfter($(lnk))
     .append('Wirklich löschen?<br>')
     .append(
-      $( '<a href="#">Ja</a>' ).on('click', function() {
+      $('<a href="#">Ja</a>').on('click', function() {
         $(this).parent().remove();
         onsuccess();
       })
     ).append(' | ')
     .append(
-      $( '<a href="#">Nein</a>' ).on('click', function() {
+      $('<a href="#">Nein</a>').on('click', function() {
         $(this).parent().remove();
         $(lnk).show();
       })
     );
 }
 
-$( document ).ready( function() {
-    var marker = [];
+$(document).ready(function() {
+  var marker = [];
 
-    var karte = L.map( 'map' );
-    L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap'
-    } ).addTo( karte );
+  var karte = L.map('map');
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+  }).addTo(karte);
 
-    $.ajax({
-      url:'http://localhost:26893/places/',
-      data:{},
-      method:'get',
-      dataFormat: 'json',
-      success:function( response ) {
-        var orte = response.places; // Array
-        if ( Array.isArray( orte ) ) {
-          console.log( orte );
-          if ( orte.length == 0 ) {
-             // keine Orte
-             karte.setView( [ 48.21, 16.38 ], 12 );
-          } else {
-             // Orte
-             var bounds = [];
+  karte.dragging.disable();
+  var locationnamestatus = false;
 
-             //karte.setView( [  orte[0].lat, orte[0].lng ], 2 );
-
-             var table = $( '<table border="1">' ).appendTo( 'body' );
-             table.append(
-               $( '<tr>' )
-                .append( $('<th>Bezeichnung</th>') )
-                .append( $('<th>Lat</th>') )
-                .append( $('<th>Lng</th>') )
-             );
-
-
-             for ( let i in orte ) {
-               var zeile = $( '<tr>' ).appendTo( table );
-               $( '<td>' ).html( orte[i].title ).appendTo( zeile );
-               $( '<td>' ).html( orte[i].lat ).appendTo( zeile );
-               $( '<td>' ).html( orte[i].lng ).appendTo( zeile );
-               $( '<td>' ).append(
-                  $( '<a href="#">' ).data('i',i ).data('id',orte[i].id ).html( 'löschen' )
-                    .on('click', function() {
-                      var id = $(this).data('id');
-                      var i = $(this).data('i');
-
-                      createConfirm( this, function() {
-
-                        $.ajax({
-                          url:'http://localhost:26893/places/'+id,
-                          method:'delete',
-                          success:function( response) {
-                            if ( response.status == 'success') {
-                              toastr["success"]("Ort wurde gelöscht.", "Erfolg");
-                              // Tabelle
-                              console.log( this );
-                              $(this).closest('tr').remove();
-                              // Marker
-                              marker[ i ].remove();
-                            } else {
-                              toastr["error"]("Ort existiert nicht mehr.", "Fehler");
-                            }
-                          }.bind(this)
-                        });
-                      }.bind(this) );
-                    })
-               ).appendTo( zeile );
-               var latlng = [
-                 orte[i].lat,
-                 orte[i].lng
-               ];
-               marker.push( L.marker(latlng)
-                .addTo( karte )
-                .bindPopup( orte[i].title ) );
-               bounds.push( latlng );
-
-
-
-
-             }
-             karte.fitBounds( bounds );
-            console.log( marker );
-          }
-
-
+  $.ajax({
+    url: 'http://localhost:26893/places/',
+    data: {},
+    method: 'get',
+    dataFormat: 'json',
+    success: function(response) {
+      var orte = response.places; // Array
+      if (Array.isArray(orte)) {
+        console.log(orte);
+        if (orte.length == 0) {
+          // keine Orte
+          karte.setView([48.21, 16.38], 12);
         } else {
-          toastr["error"]("Datenformat Fehler", "Fehler");
+          // Orte
+          var bounds = [];
+
+          //karte.setView( [  orte[0].lat, orte[0].lng ], 2 );
+
+          var table = $('<table border="1">').css({
+            margin: 10
+          }).appendTo('#inputcontainer');
+          table.append(
+            $('<tr>')
+            .append($('<th>Bezeichnung</th>'))
+            .append($('<th>Lat</th>'))
+            .append($('<th>Lng</th>'))
+          );
+
+
+          for (let i in orte) {
+            var zeile = $('<tr>').appendTo(table);
+            $('<td>').html(orte[i].title).appendTo(zeile);
+            $('<td>').html(orte[i].lat).appendTo(zeile);
+            $('<td>').html(orte[i].lng).appendTo(zeile);
+            $('<td>').append(
+              $('<a href="#">').data('i', i).data('id', orte[i].id).html('löschen')
+              .on('click', function() {
+                var id = $(this).data('id');
+                var i = $(this).data('i');
+
+                createConfirm(this, function() {
+
+                  $.ajax({
+                    url: 'http://localhost:26893/places/' + id,
+                    method: 'delete',
+                    success: function(response) {
+                      if (response.status == 'success') {
+                        toastr["success"]("Ort wurde gelöscht.", "Erfolg");
+                        // Tabelle
+                        console.log(this);
+                        $(this).closest('tr').remove();
+                        // Marker
+                        marker[i].remove();
+                      } else {
+                        toastr["error"]("Ort existiert nicht mehr.", "Fehler");
+                      }
+                    }.bind(this)
+                  });
+                }.bind(this));
+              })
+            ).appendTo(zeile);
+            var latlng = [
+              orte[i].lat,
+              orte[i].lng
+            ];
+            marker.push(L.marker(latlng)
+              .addTo(karte)
+              .bindPopup(orte[i].title));
+            bounds.push(latlng);
+
+
+
+
+          }
+          karte.fitBounds(bounds);
+          console.log(marker);
         }
-      },
-      error:function() {
-        toastr["error"]("Verbindunsfehler", "Fehler");
+
+
+      } else {
+        toastr["error"]("Datenformat Fehler", "Fehler");
       }
-    });
+    },
+    error: function() {
+      toastr["error"]("Verbindunsfehler", "Fehler");
+    }
+  });
+
+  $('#inputnamemarker').focusout(function() {
+
+    $(this).removeClass('fail');
+    if (this !== "") {
+      karte.dragging.enable();
+      locationnamestatus = true;
+    }
+
+  })
+  karte.on('click', function(e) {
+
+    if (locationnamestatus == false) {
+      console.log('testkakskd');
+      $('#inputnamemarker').addClass('fail');
+    }
+
+    $("#inputlatcoord").val(e.latlng.lat);
+    $("#inputlngcoord").val(e.latlng.lng);
+  })
+
+ $('#btnnewLocation').on('click',function(){
+
+   $.ajax({
+     url: "http://localhost:26893/places/",
+     method: "POST",
+     dataFormat: "json",
+     data:{
+         "title": $('#inputnamemarker').val(),
+         "lat": $('#inputlatcoord').val(),
+         "lng": $('#inputlngcoord').val()
+     },
+     success: function(response){
+
+     }
+
+
+
+
+   })
+
+ })
+
+
+
+
 
 });
